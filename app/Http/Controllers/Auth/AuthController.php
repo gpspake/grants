cb76d455f822eec2a70f0de9832a904ccd822504
gpspake <gpspake@gmail.com>
2015-07-23 10:48:39 -0500

    Update auth controller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -20,10 +20,10 @@ class AuthController extends Controller
     |
     */
 
-    use AuthenticatesUsers
+    use AuthenticatesUsers;
 
     protected $redirectAfterLogout = '/auth/login';
-    protected $redirectTo = '/admin/post';
+    protected $redirectTo = '/admin/grant';
 
     /**
      * Create a new authentication controller instance.
