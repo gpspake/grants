955606cb8b24ea4293f806da8bfd089199530b81
gpspake <gpspake@gmail.com>
2015-07-22 20:59:29 -0500

    add admin routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,3 +6,21 @@ get('/', function () {
 
 get('grants', 'GrantController@index');
 get('grants/{slug}', 'GrantController@showGrant');
+
+// Admin area
+get('admin', function () {
+    return redirect('/admin/grant');
+});
+$router->group([
+    'namespace' => 'Admin',
+    'middleware' => 'auth',
+], function () {
+    resource('admin/grant', 'GrantController');
+    resource('admin/tag', 'TagController');
+    get('admin/upload', 'UploadController@index');
+});
+
+// Logging in and out
+get('/auth/login', 'Auth\AuthController@getLogin');
+post('/auth/login', 'Auth\AuthController@postLogin');
+get('/auth/logout', 'Auth\AuthController@getLogout');
