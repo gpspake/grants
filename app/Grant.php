83eb5d0e8907d2e61bf7cda3ad56ac131802e92d
gpspake <gpspake@gmail.com>
2015-07-27 21:22:24 -0500

    update grant model

--- app/Grant.php
+++ app/Grant.php
@@ -1,19 +1,126 @@
 <?php
-
 namespace App;
 
+use App\Services\Markdowner;
 use Illuminate\Database\Eloquent\Model;
 
 class Grant extends Model
 {
     protected $dates = ['published_at'];
+    protected $fillable = [
+        'title',
+        'subtitle',
+        'meta_description',
+        'description_raw',
+        'maker',
+        'maker_website',
+        'program',
+        'program_website',
+        'maximum_award',
+        'layout',
+        'is_draft',
+        'letter_of_intent_deadline',
+        'limited_submission_deadline',
+        'published_at'
+    ];
 
+
+    /**
+     * The many-to-many relationship between grants and tags.
+     *
+     * @return BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag', 'grant_tag_pivot');
+    }
+
+    /**
+     * Set the title attribute and automatically the slug
+     *
+     * @param string $value
+     */
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
 
         if (! $this->exists) {
-            $this->attributes['slug'] = str_slug($value);
+            $this->setUniqueSlug($value, '');
         }
     }
+
+    /**
+     * Recursive routine to set a unique slug
+     *
+     * @param string $title
+     * @param mixed $extra
+     */
+    protected function setUniqueSlug($title, $extra)
+    {
+        $slug = str_slug($title.'-'.$extra);
+
+        if (static::whereSlug($slug)->exists()) {
+            $this->setUniqueSlug($title, $extra + 1);
+            return;
+        }
+
+        $this->attributes['slug'] = $slug;
+    }
+
+    /**
+     * Set the HTML content automatically when the raw content is set
+     *
+     * @param string $value
+     */
+    public function setDescriptionRawAttribute($value)
+    {
+        $markdown = new Markdowner();
+
+        $this->attributes['description_raw'] = $value;
+        $this->attributes['description_html'] = $markdown->toHTML($value);
+    }
+
+    /**
+     * Sync tag relation adding new tags as needed
+     *
+     * @param array $tags
+     */
+    public function syncTags(array $tags)
+    {
+        Tag::addNeededTags($tags);
+
+        if (count($tags)) {
+            $this->tags()->sync(
+                Tag::whereIn('tag', $tags)->lists('id')->all()
+            );
+            return;
+        }
+
+        $this->tags()->detach();
+    }
+
+    /**
+     * Return the date portion of published_at
+     */
+    public function getPublishDateAttribute($value)
+    {
+        return $this->published_at->format('M-j-Y');
+    }
+
+    /**
+     * Return the time portion of published_at
+     */
+    public function getPublishTimeAttribute($value)
+    {
+        return $this->published_at->format('g:i A');
+    }
+
+    /**
+     * Alias for content_raw
+     */
+    public function getDescriptionAttribute($value)
+    {
+        return $this->description_raw;
+    }
+
 }
