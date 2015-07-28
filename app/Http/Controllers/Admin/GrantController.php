fbda4ad4e93c508441807193b02e9ecaca65c9b4
gpspake <gpspake@gmail.com>
2015-07-27 21:47:09 -0500

    update grant controller

--- app/Http/Controllers/Admin/GrantController.php
+++ app/Http/Controllers/Admin/GrantController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
-
+use App\Jobs\GrantFormFields;
 use App\Http\Requests;
+use App\Http\Requests\GrantCreateRequest;
+use App\Http\Requests\GrantUpdateRequest;
 use App\Http\Controllers\Controller;
+use App\Grant;
 
 class GrantController extends Controller
 {
@@ -16,7 +18,8 @@ class GrantController extends Controller
      */
     public function index()
     {
-        return view('admin.grant.index');
+        return view('admin.grant.index')
+            ->withGrants(Grant::all());
     }
 
     /**
@@ -26,18 +29,25 @@ class GrantController extends Controller
      */
     public function create()
     {
-        //
+        $data = $this->dispatch(new GrantFormFields());
+
+        return view('admin.grant.create', $data);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request  $request
+     * @param  GrantCreateRequest $request
      * @return Response
      */
-    public function store(Request $request)
+    public function store(GrantCreateRequest $request)
     {
-        //
+        $grant = Grant::create($request->grantFillData());
+        $grant->syncTags($request->get('tags', []));
+
+        return redirect()
+            ->route('admin.grant.index')
+            ->withSuccess('New Grant Successfully Entered.');
     }
 
     /**
@@ -59,19 +69,34 @@ class GrantController extends Controller
      */
     public function edit($id)
     {
-        //
+        $data = $this->dispatch(new GrantFormFields($id));
+
+        return view('admin.grant.edit', $data);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  Request  $request
+     * @param  GrantUpdateRequest $request
      * @param  int  $id
      * @return Response
      */
-    public function update(Request $request, $id)
+    public function update(GrantUpdateRequest $request, $id)
     {
-        //
+        $grant = Grant::findOrFail($id);
+        $grant->fill($request->grantFillData());
+        $grant->save();
+        $grant->syncTags($request->get('tags', []));
+
+        if ($request->action === 'continue') {
+            return redirect()
+                ->back()
+                ->withSuccess('Grant saved.');
+        }
+
+        return redirect()
+            ->route('admin.grant.index')
+            ->withSuccess('Grant saved.');
     }
 
     /**
@@ -82,6 +107,12 @@ class GrantController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $grant = Grant::findOrFail($id);
+        $grant->tags()->detach();
+        $grant->delete();
+
+        return redirect()
+            ->route('admin.grant.index')
+            ->withSuccess('Grant deleted.');
     }
 }
