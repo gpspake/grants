a2910553410e2211ece18afe1743d85a4c215560
gpspake <gpspake@gmail.com>
2015-07-27 21:24:20 -0500

    Update tag model

--- app/Tag.php
+++ app/Tag.php
@@ -1,10 +1,46 @@
 <?php
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = [
+        'tag', 'title', 'subtitle', 'meta_description', 'reverse_direction'
+    ];
+
+    /**
+     * The many-to-many relationship between tags and posts.
+     *
+     * @return BelongsToMany
+     */
+    public function grants()
+    {
+        return $this->belongsToMany('App\Grant', 'grant_tag_pivot');
+    }
+
+    /**
+     * Add any tags needed from the list
+     *
+     * @param array $tags List of tags to check/add
+     */
+    public static function addNeededTags(array $tags)
+    {
+        if (count($tags) === 0) {
+            return;
+        }
+
+        $found = static::whereIn('tag', $tags)->lists('tag')->all();
+
+        foreach (array_diff($tags, $found) as $tag) {
+            static::create([
+                'tag' => $tag,
+                'title' => $tag,
+                'subtitle' => 'Subtitle for '.$tag,
+                'page_image' => '',
+                'meta_description' => '',
+                'reverse_direction' => false,
+            ]);
+        }
+    }
 }
