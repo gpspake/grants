20711d7d6b7a8bcdada62a476f97a08bb3ce8ae0
gpspake <gpspake@gmail.com>
2015-08-01 09:16:38 -0500

    add additional settings to grants config

--- config/grants.php
+++ config/grants.php
@@ -1,5 +1,13 @@
 <?php
 return [
-    'title' => 'CFR Site',
-    'grants_per_page' => 10
+    'name' => "Grants",
+    'title' => 'Grants Site',
+    'subtitle' => 'A grants app written in Laravel 5.1',
+    'description' => 'This is my meta description',
+    'author' => 'George Spake',
+    'grants_per_page' => 10,
+    'uploads' => [
+        'storage' => 'local',
+        'webpath' => '/uploads/',
+    ],
 ];
