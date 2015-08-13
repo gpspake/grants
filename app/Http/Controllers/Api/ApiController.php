9f3a6366010c5266ee647dfc625531fc32118431
gpspake <gpspake@gmail.com>
2015-08-12 21:46:58 -0500

    add api controller

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,64 @@
+<?php namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class ApiController extends Controller {
+
+    /**
+     * @var int
+     */
+    protected $statusCode = 200;
+
+    /**
+     * @return mixed
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+
+    /**
+     * @param $statusCode
+     * @return $this
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
+    /**
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function respondNotFound($message = 'Not found')
+    {
+        return $this->setStatusCode(404)->respondWithError($message);
+    }
+
+    /**
+     * @param $data
+     * @param array $headers
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function respond($data, $headers = [])
+    {
+        return response()->json($data, $this->getStatusCode(), $headers);
+    }
+
+    /**
+     * @param $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function respondWithError($message)
+    {
+        return $this->respond([
+            'error' => [
+                'message' => $message,
+                'status_code' => $this->getStatusCode()
+            ]
+        ]);
+    }
+}
