4b7cb61527d1e9729b335f96b8f5706ec4cdf20d
gpspake <gpspake@gmail.com>
2015-09-13 09:59:39 -0500

    re-order grant table seeder calls to fix error

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call('UserTableSeeder');
-        $this->call('GrantTableSeeder');
         $this->call('TagTableSeeder');
+        $this->call('GrantTableSeeder');
 
         Model::reguard();
     }
