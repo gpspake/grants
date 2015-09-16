b254e3627afaf15159977cb00d03724de90aea87
gpspake <gpspake@gmail.com>
2015-09-15 22:28:21 -0500

    refactor html and form aliases

--- config/app.php
+++ config/app.php
@@ -193,8 +193,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Html'      => Illuminate\Html\HtmlFacade::class,
-        'Form'      => Illuminate\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Form'      => Collective\Html\FormFacade::class,
         'Cas'       => Xavrsl\Cas\Facades\Cas::class
     ],
 
