9d2e36b87bfa6a5a550ea9a55a39d532ccfb79ac
gpspake <gpspake@gmail.com>
2015-09-21 23:58:31 -0500

    restructure users table

---
+++ database/migrations/2015_09_21_225409_restructure_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RestructureUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('name', 'netid');
+            $table->string('first_name')->after('name');
+            $table->string('last_name')->after('first_name');
+            $table->string('display_name')->after('last_name');
+            $table->dropColumn('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('password', 60);
+            $table->dropColumn('display_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('first_name');
+            $table->renameColumn('netid', 'name');
+        });
+    }
+}
